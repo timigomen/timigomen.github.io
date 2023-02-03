2601e4e62baacaab8a0d9724fe07e1d2cd924217
timigomen <3521614880@qq.com>
2023-02-03 17:48:30 +0800

    Site updated: 2023-02-03 17:48:29

--- js/custom/custom.js
+++ js/custom/custom.js
@@ -1,18 +1,76 @@
 "" === GLOBAL_CONFIG_SITE.title.replace("Jayhrn", "") ? document.getElementById("page-name-text").style.display = "none" : document.querySelector("#page-name-text>span").innerHTML = document.title.split(" | Jayhrn")[0];
+// 存数据
+function saveData(name, data) { localStorage.setItem(name, JSON.stringify({ 'time': Date.now(), 'data': data })) };
+// 取数据
+function loadData(name, time) {
+    let d = JSON.parse(localStorage.getItem(name));
+    // 过期或有错误返回 0 否则返回数据
+    if (d) {
+        let t = Date.now() - d.time
+        if (-1 < t && t < (time * 60000)) return d.data;
+    }
+    return 0;
+};
 
-function postAddToc() {
-    let postContent = document.querySelector('#post>#article-container.post-content')
-    let cardToc = document.getElementById('card-toc')
-    if (postContent && cardToc) {
-        let tocNumber = cardToc.getElementsByClassName('toc-number')
-        let tocLink = cardToc.getElementsByClassName('toc-link')
-        for (let i = 0; i < tocLink.length; i++) {
-            document.getElementById(decodeURIComponent(tocLink[i].attributes.href.value).slice(1)).dataset.toc = tocNumber[i].innerText
-        }
+let talkTimer = null;
+function indexTalk() {
+    if (talkTimer) {
+        clearInterval(talkTimer)
+        talkTimer = null;
+    }
+    if (!document.getElementById('bber-talk')) return
+
+    function toText(ls) {
+        let text = []
+        ls.forEach(item => {
+            text.push(item.content.replace(/#(.*?)\s/g, '').replace(/\{(.*?)\}/g, '').replace(/\!\[(.*?)\]\((.*?)\)/g, '<i class="fa-solid fa-image"></i>').replace(/(?<!!)\[(.*?)\]\((.*?)\)/g, '<i class="fa-solid fa-link"></i>'))
+        });
+        return text
+    }
+
+    function talk(ls) {
+        let html = ''
+        ls.forEach((item, i) => { html += `<li class="item item-${i + 1}">${item}</li>` });
+        let box = document.querySelector("#bber-talk .talk-list")
+        box.innerHTML = html;
+        talkTimer = setInterval(() => {
+            box.appendChild(box.children[0]);
+        }, 3000);
+    }
+
+    let d = loadData('talk', 10);
+    if (d) talk(d);
+    else {
+        fetch('https://memos.itisn.cyou/api/memo?openId=0c73bab0-e69d-4744-b59b-e678890f0ec1&tag=%E8%AF%B4%E8%AF%B4&limit=30creatorId=1').then(res => res.json()).then(data => { // 更改地址
+            data = toText(data.data)
+            talk(data);
+            saveData('talk', data);
+        })
     }
 }
-postAddToc() // 打开网站先执行一次
-document.addEventListener("pjax:complete", postAddToc) // pjax加载完成（切换页面）后再执行一次
+
+// pjax注释掉上面的 indexTalk(); 使用如下方法：
+function whenDOMReady() {
+	let postContent = document.querySelector('#post>#article-container.post-content')
+	let cardToc = document.getElementById('card-toc')
+	if (postContent && cardToc) {
+	    let tocNumber = cardToc.getElementsByClassName('toc-number')
+	    let tocLink = cardToc.getElementsByClassName('toc-link')
+	    for (let i = 0; i < tocLink.length; i++) {
+	        document.getElementById(decodeURIComponent(tocLink[i].attributes.href.value).slice(1)).dataset.toc = tocNumber[i].innerText
+	    }
+	}
+    indexTalk();
+	var $percent = document.querySelector("#nav #hotkey #top-button a.site-page i");
+	$percent && window.addEventListener("scroll", (function () {
+	    let e = document.body.scrollHeight || document.documentElement.scrollHeight,
+	        t = window.innerHeight || document.documentElement.clientHeight;
+	    $percent.dataset.percent = ((document.body.scrollTop || document.documentElement.scrollTop) / (e - t) * 100).toFixed(0)
+	}));
+}
+
+whenDOMReady()
+document.addEventListener("pjax:complete", whenDOMReady)
 
 postAddToc()
 // 检测按键
@@ -183,56 +241,6 @@ $(document).unbind('keydown').bind('keydown', function (e) {
         return false;
     }
 })
-
-let newYearTimer = null;
-var newYear = () => {
-    clearTimeout(newYearTimer);
-    if (!document.querySelector('#newYear')) return;
-    // 新年时间戳 and 星期对象
-    let newYear = new Date('2023-01-22 00:00:00').getTime() / 1000,
-        week = { 0: '周日', 1: '周一', 2: '周二', 3: '周三', 4: '周四', 5: '周五', 6: '周六' }
-
-    time();
-
-    // 补零函数
-    function nol(h) { return h > 9 ? h : '0' + h; };
-
-    function time() {
-        // 现在 时间对象
-        let now = new Date();
-
-        // 右下角 今天
-        document.querySelector('#newYear .today').innerHTML = now.getFullYear() + '-' + (now.getMonth() + 1) + '-' + now.getDate() + ' ' + week[now.getDay()]
-
-        // 现在与新年相差秒数
-        let second = newYear - Math.round(now.getTime() / 1000);
-
-        // 小于0则表示已经过年
-        if (second < 0) {
-            document.querySelector('#newYear .title').innerHTML = 'Happy New Year!';
-            document.querySelector('#newYear .newYear-time').innerHTML = '<span class="happyNewYear">新年快乐</span>';
-        } else {
-            // 大于0则还未过年
-            document.querySelector('#newYear .title').innerHTML = '距离2023年春节：'
-
-            // 大于一天则直接渲染天数
-            if (second > 86400) {
-                document.querySelector('#newYear .newYear-time').innerHTML = `<span class="day">${Math.ceil(second / 86400)}<span class="unit">天</span></span>`
-            } else {
-                // 小于一天则使用时分秒计时。
-                let h = nol(parseInt(second / 3600));
-                second %= 3600;
-                let m = nol(parseInt(second / 60));
-                second %= 60;
-                let s = nol(second);
-                document.querySelector('#newYear .newYear-time').innerHTML = `<span class="time">${h}:${m}:${s}</span></span>`;
-                // 计时
-                newYearTimer = setTimeout(time, 1000);
-            }
-        }
-    }
-}
-
 
 function doStuff() {
     var flag = 0;
@@ -756,19 +764,6 @@ var heo = {
         $('body').addClass('pace-done')
     }
 }
-function whenDOMReady() {
-    newYear();
-    var $percent = document.querySelector("#nav #hotkey #top-button a.site-page i");
-    $percent && window.addEventListener("scroll", (function () {
-        let e = document.body.scrollHeight || document.documentElement.scrollHeight,
-            t = window.innerHeight || document.documentElement.clientHeight;
-        $percent.dataset.percent = ((document.body.scrollTop || document.documentElement.scrollTop) / (e - t) * 100).toFixed(0)
-    }));
-}
-
-whenDOMReady() // 打开网站先执行一次
-document.addEventListener("pjax:complete", whenDOMReady) // pjax加载完成（切换页面）后再执行一次
-
 
 function fullScreen() {
     var de = document.documentElement;
